eb4bb07f6d3b7711536f426f85257959b0255c10
ks5435 <S.Kirty@corp.sysco.com>
2018-08-09 18:26:27 +0530

    Updates

--- venv/Practise/Training3.py
+++ venv/Practise/Training3.py
@@ -1,7 +1,7 @@
 #With a given integral number n, write a program to generate a dictionary that contains
 #(i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
 
-x = input ('entre the number: ')
+x = input ("entre the number: ")
 n= int(x)
 d=dict()
 for i in range(1,n+1):
